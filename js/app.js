4b1a0ff23970529b39107402a5efd085d311691e
Kundan Singh <kundan.singh979@gmail.com>
2017-07-31 17:34:02 +0530

    Routing and partials

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider){
+	$routeProvider
+		.when("/books",{
+			templateUrl: 'partials/book-list.html',
+			controller: 'BookListCtrl'
+		})
+		.when("/genres",{
+			templateUrl: 'partials/genre-list.html'
+		})
+		.otherwise({
+			redirectTo: "/books"
+		});
+});
 
 myApp.controller('NavbarCtrl', function ($scope) {
 	$scope.appDetails = {
@@ -7,6 +21,10 @@ myApp.controller('NavbarCtrl', function ($scope) {
 });
 
 myApp.controller('BookListCtrl', function($scope){
+	$scope.addToFavourites = function(book){
+		console.log('Add to Favourite');
+		console.log(book);
+	}
 	$scope.books = [
 		{
 			imgUrl: '',
